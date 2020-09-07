15b027f1591c0cfcafd1a348bcf7e0b3275460a2
Michael Kaiser <michael.kaiser@conplement.de>
2020-09-07 23:25:12 +0200

    add date picker

--- power-usage/power_usage/lib/screens/monthly_usage_screen.dart
+++ power-usage/power_usage/lib/screens/monthly_usage_screen.dart
@@ -3,10 +3,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../widgets/power_usage_tile.dart';
 import '../state/usages.dart';
+import 'package:month_picker_dialog/month_picker_dialog.dart';
 
 class MonthlyUsageScreen extends StatelessWidget {
   static const String routeName = "/monthlyusages";
   final loadedPowerUsage = POWER_USAGES;
+  DateTime datePicked = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +24,46 @@ class MonthlyUsageScreen extends StatelessWidget {
               Expanded(
                 child: Container(
                   color: Colors.lightBlue,
-                  height: 150,
+                  height: 70,
+                  alignment: Alignment.center,
+                  child: Container(
+                    height: 50,
+                    width: 200,
+                    child: RaisedButton(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),
+                        side: BorderSide(color: Colors.red),
+                      ),
+                      textColor: Colors.white,
+                      color: Colors.red,
+                      onPressed: () async {
+                        final picked = await showMonthPicker(
+                            context: context, initialDate: datePicked);
+                        datePicked = picked != null ? picked : datePicked;
+                        print(picked);
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          const Icon(
+                            Icons.calendar_today,
+                            color: Colors.white,
+                            size: 24.0,
+                          ),
+                          Flexible(
+                            child: const Text(
+                              'Datum auswählen',
+                              overflow: TextOverflow.ellipsis,
+                              softWrap: false,
+                              style: TextStyle(
+                                fontSize: 15,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
                 ),
               ),
             ],
@@ -30,10 +71,10 @@ class MonthlyUsageScreen extends StatelessWidget {
           Expanded(
             child: Container(
                 child: GridView.count(
-              padding: EdgeInsets.symmetric(horizontal: 60, vertical: 10),
+              padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
               crossAxisCount: 2,
               crossAxisSpacing: 50,
-              mainAxisSpacing: 20,
+              mainAxisSpacing: 30,
               childAspectRatio: 1 / 1,
               children: [
                 PowerUsageTile(
