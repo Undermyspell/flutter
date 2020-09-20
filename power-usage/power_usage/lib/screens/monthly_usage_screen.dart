98df2c2282579e105cd949db74349f9a7b6c0305
Undermyspell <michael_kaiser2@outlook.com>
2020-09-20 20:17:54 +0200

    adds dialog

--- power-usage/power_usage/lib/screens/monthly_usage_screen.dart
+++ power-usage/power_usage/lib/screens/monthly_usage_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 
 import 'package:flutter/material.dart';
@@ -82,7 +84,45 @@ class _MonthlyUsageScreenState extends State<MonthlyUsageScreen> {
           Icons.add,
           color: Colors.white,
         ),
-        onPressed: () {},
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) => new BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                child: AlertDialog(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.0)),
+                  backgroundColor: Colors.white,
+                  content: Container(
+                    height: MediaQuery.of(context).size.height / 2,
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Please enter some text';
+                            }
+                            return null;
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                  actions: [
+                    FlatButton(
+                      child: Text("ok"),
+                      onPressed: () {},
+                    ),
+                    FlatButton(
+                      child: Text("cancel"),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    )
+                  ],
+                )),
+          );
+        },
         backgroundColor: Theme.of(context).accentColor,
       ),
       bottomNavigationBar: BottomAppBar(
